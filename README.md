bdfbe07a5c8350e1d43dd157f13d7a6674a14fbc
tteenz <trantiinaa@yahoo.com>
2019-03-09 23:34:49 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # FriendFinder
+
+Basically a dating app however for friends! 
+
+This site will take results from user's input on a survey. 
+
+The survey will display 10 questions in which user chooses 1 for strongly disagree or 5 for strongly agree. 
+
+Once the user submit their inputs, the app will then display a name and picture that they are most compatible with! 
+
+Go ahead and check it out ! 
+https://peaceful-stream-89522.herokuapp.com
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,4 @@
 // Dependencies
-var path = require('path');
 var friends = require('../data/friends.js');
 
 // Route
@@ -10,8 +9,8 @@ module.exports = function (app) {
     });
     app.post("/api/friends", function (req, response) {
         var difference = 40;
-        var matchName = '';
-        var matchPhoto = '';
+        var friendName = '';
+        var friendPhoto = '';
 
         friends.forEach(function (friend) {
             var matchedScoresArray = [];
@@ -30,62 +29,15 @@ module.exports = function (app) {
 
             if (totalDifference < difference) {
                 difference = totalDifference;
-                matchName = friend.name;
-                matchPhoto = friend.photo;
+                friendName = friend.name;
+                friendPhoto = friend.photo;
             }
         });
         response.json({
-            name: matchName,
-            photo: matchPhoto
+            name: friendName,
+            photo: friendPhoto
         });
 
         friends.push(req.body);
     });
 }
-
-
-
-
-// 		var newFriend = request.body;
-// 		//console.log(friends[0].score[0]);
-
-//  //console.log(newFriend);
-
-//         for(var i = 0; i < newFriend.scores.length; i++){
-//             if(newFriend.scores[i] === "1 (Strongly Disagree)"){
-//                 newFriend.scores[i] = 1;
-//             }else if(newFriend.scores[i] === "5 (Strongly Agree)"){
-//                 newFriend.scores[i] = 5;
-//             }else {
-//                 newFriend.scores[i] = parseInt(newFriend.scores[i]);
-//             }
-//         }
-
-//         var diff = [];
-
-//         for(var i = 0; i < friends.length; i++){
-//             var compatibleFriend = friends[i];
-//             var totalDifference = 0;
-
-//             for(var j = 0; j < compatibleFriend.scores.length; j++){
-//                 var tempDiff = Math.abs(compatibleFriend.scores[j] - newFriend.scores[j]);
-//                 totalDifference += tempDiff; 
-//             }
-//             diff.push(totalDifference);
-//         }
-
-//         var num = diff[0];
-//         var index = 0;
-
-//         for(var x = 0; x < diff.length; x++){
-//             if(diff[x] < num){
-//                 num = diff[x];
-//                 index = x;
-//             }
-//         }
-
-//         friends.push(newFriend);
-
-//         response.json(friends[index]);
-//     });
-// }
